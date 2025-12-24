9eafaeef85dd4946dd5ed0270adc2b8b41b58f3f
Aditya Kumar <kumarsinghaditya240@gmail.com>
2025-12-25 00:27:51 +0530

    Implement authentication context with login and logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Create the context
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if(storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  // Login function
+  const login = async (email, password) => {
+    try {
+      // send request to the backend
+      const {data} = await axios.post("http://localhost:5000/api/auth/login",
+        {
+          email,
+          password
+        });
+
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        return {success: true};
+    }catch (error) {
+      return {
+        success: false,
+        error: error.response?.data?.message || "Login failed"
+      };
+    }
+  };
+
+  // Logout function
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AuthContext.Provider value = {{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-export default App
+export default AuthContext;
