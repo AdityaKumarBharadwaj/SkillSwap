d4e5c66d01738fb204fb58979d869bde88a1b8a0
Aditya Kumar <kumarsinghaditya240@gmail.com>
2025-12-25 01:21:02 +0530

    Add authentication routes for user registration and login

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 // 1. Load environment variables
 dotenv.config();
@@ -14,7 +15,7 @@ const app = express();
 // 3. Middleware (The Gatekeepers)
 app.use(express.json);    // Allows server to accept JSON data in body
 app.use(cors());        // Allows React to talk to this server
-
+app.use('/api/auth', authRoutes);
 
 // 4. API routes
 app.get('/', (req, res) => {
