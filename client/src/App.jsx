ad8d65d312025f63715c241adb7c463c9e1bcf11
Aditya Kumar <kumarsinghaditya240@gmail.com>
2025-12-25 01:28:48 +0530

    mistake made in AuthContext.jsx correct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,11 @@
-import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react'
 
-// Create the context
-const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if(storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
-
-  // Login function
-  const login = async (email, password) => {
-    try {
-      // send request to the backend
-      const {data} = await axios.post("http://localhost:5000/api/auth/login",
-        {
-          email,
-          password
-        });
-
-        setUser(data);
-        localStorage.setItem("user", JSON.stringify(data));
-        return {success: true};
-    }catch (error) {
-      return {
-        success: false,
-        error: error.response?.data?.message || "Login failed"
-      };
-    }
-  };
-
-  // Logout function
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-  };
-
+const App = () => {
   return (
-    <AuthContext.Provider value = {{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+    <div>
+      
+    </div>
+  )
+}
 
-export default AuthContext;
+export default App
