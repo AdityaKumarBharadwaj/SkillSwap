5ec6505f1961a785b169d3014b20a6adf0365c75
Aditya Kumar <kumarsinghaditya240@gmail.com>
2025-12-27 23:48:36 +0530

    Implemented App.jsx component to include AuthProvider and routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { AuthProvider } from './context/AuthContext'
+import Navbar from './components/Navbar'
+import {Routes, Route } from 'react-router-dom'
+import { Home } from 'lucide-react'
 
 const App = () => {
   return (
     <div>
-      
+      <AuthProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-50 font-sans">
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/login' element={<Login />} />
+              <Route path='.register' element={<Register />}/>
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
     </div>
   )
 }
