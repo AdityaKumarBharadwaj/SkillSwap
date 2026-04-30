4e619dce111dff340bca0b6b2d245fced60848c6
Aditya Kumar <kumarsinghaditya240@gmail.com>
2026-04-30 23:19:24 +0530

    configure server for production static serving and add root scripts

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const skillRoutes = require('./routes/skillRoutes');
-
+const path = require('path');
 
 // 1. Load environment variables
 dotenv.config();
@@ -18,14 +18,22 @@ const app = express();
 app.use(express.json());    // Allows server to accept JSON data in body
 app.use(cors());        // Allows React to talk to this server
 app.use('/api/auth', authRoutes);
-app.use('/api', authRoutes);
 app.use('/api/skills', skillRoutes);
 
-// 4. API routes
-app.get('/', (req, res) => {
-    res.send('Api is running....');
-})
+// 4. Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../client', 'dist', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
